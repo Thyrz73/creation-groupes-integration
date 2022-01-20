bf13f6e4a98d7057730d481db72a9af9449a64a7
Guillaume Luquet <44996746+Thyrz73@users.noreply.github.com>
2022-01-20 15:14:28 +0100

    Update Jekinsfile

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -1,10 +1,28 @@
 pipeline {
-    agent { docker { image 'node:14-alpine' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'npm --version'
-            }
-        }
+  agent {
+     label 'windows'
+  }
+    options{
+        buildDiscarder logRotator(artifactDaysToKeepStr: '',  artifactNumToKeepStr: '5',  daysToKeepStr: '', numToKeepStr: '5')
+        disableConcurrentBuilds()
+  stages {
+    stage('Install') {
+      steps { sh 'npm install' }
     }
+
+    stage('Test') {
+      parallel {
+        stage('Static code analysis') {
+            steps { sh 'npm run-script lint' }
+        }
+        stage('Unit tests') {
+            steps { sh 'npm run-script test' }
+        }
+      }
+    }
+
+    stage('Build') {
+      steps { sh 'npm run-script build' }
+    }
+  }
 }
