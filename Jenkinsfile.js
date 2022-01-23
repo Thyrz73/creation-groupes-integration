20662b49947f56b86565605bca2e88ed019f0b4d
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2022-01-23 18:29:14 +0100

    Update Jenkinsfile.js

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -1,13 +1,3 @@
-// pipeline {
-//     agent { docker { image 'node:14-alpine' } }
-//     stages {
-//         stage('build') {
-//             steps {
-//                 sh 'npm --version'
-//             }
-//         }
-//     }
-// }
 pipeline {
   agent any
   tools {nodejs "nodejs"}
