9ddc4006cb212af4f1fcc656c5ab51bf30b2decc
Guillaume <guillaume.ldsg@gmail.com>
2022-01-23 23:55:58 +0100

    ajout credit firebase environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCB_aRfaA9dKbbO0o1T13qHSMU4zrB4y5A",
+    authDomain: "intg-continue.firebaseapp.com",
+    databaseURL: "https://intg-continue.firebaseio.app/",
+    projectId: "intg-continue",
+    storageBucket: "intg-continue.appspot.com",
+    messagingSenderId: "146510333686",
+    appId: "1:146510333686:web:bfbb4d349ad5c992a34ed7"
+  }
 };
