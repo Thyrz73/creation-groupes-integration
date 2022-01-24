9e131b4626d0cca257877cc2c9bf5f854270987c
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2022-01-24 01:30:40 +0100

    fix test

--- src/app/components/infos/infos.component.spec.ts
+++ src/app/components/infos/infos.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfosComponent } from './infos.component';
+import { DatabaseService } from 'src/app/services/database.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 describe('InfosComponent', () => {
   let component: InfosComponent;
@@ -8,7 +11,9 @@ describe('InfosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InfosComponent ]
+      declarations: [ InfosComponent ],
+      imports: [AngularFireModule.initializeApp(environment.firebaseConfig)],
+      providers: [ DatabaseService ]
     })
     .compileComponents();
   });
