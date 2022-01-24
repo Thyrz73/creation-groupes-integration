4185d9e75df9a65f5ec790811eb5de4d8115eac5
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2022-01-24 01:31:44 +0100

    fix test

--- src/app/components/admin/config-grpes/config-grpes.component.spec.ts
+++ src/app/components/admin/config-grpes/config-grpes.component.spec.ts
@@ -4,6 +4,8 @@ import { ConfigGrpesComponent } from './config-grpes.component';
 import { DatabaseService } from 'src/app/services/database.service';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 
 describe('ConfigGrpesComponent', () => {
@@ -13,7 +15,7 @@ describe('ConfigGrpesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ConfigGrpesComponent ],
-      imports: [AngularFireModule.initializeApp(environment.firebaseConfig)],
+      imports: [AngularFireModule.initializeApp(environment.firebaseConfig), RouterTestingModule],
       providers: [ DatabaseService ]
     })
     .compileComponents();
